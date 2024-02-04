4db4d06055af0628158e41b1d526cff79a24262e
Adarsh Bhardwaj <adarshbhardwaj285@gmail.com>
2024-02-04 20:56:08 +0530

    Orionix Clone

---
+++ assets/js/menu.js
@@ -0,0 +1,12 @@
+const hamburger_menu = document.querySelector("#hamburger-menu");
+const close_menu = document.querySelector("#close-menu");
+const menu = document.querySelector("#navmenu");
+
+hamburger_menu.addEventListener("click", () => {
+    menu.classList.add("show");
+});
+
+close_menu.addEventListener("click", () => {
+    menu.classList.remove("show");
+});
+
